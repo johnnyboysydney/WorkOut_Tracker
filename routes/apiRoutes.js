4861c1cc1782fedeb1c44338ef17c19ee7f20a2f
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-13 08:19:25 +1000

    Adding app.get, app.post, app.put

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,30 @@
 const db = require("../models")
+
+module.exports = function (app) {
+    app.get("/api/workouts", function(req, res){
+        Workout.find()
+        .then(data => {res.json(data)})
+        .catch(err =>  {res.json(err)})
+    });
+
+    app.post("/api/workouts", function (req, res) {
+        Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log("err", err)
+            res.json(err)
+        })
+    });
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
+    });
+}
