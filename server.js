606ef238d833cd35c02c765f8bb2d803ee9fb241
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-12 20:52:45 +1000

    Adding the server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+// Dependencies and variables
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+
+// Start the server to begin listening
+app.listen(PORT, function() {
+    console.log('App listening on Port ${PORT}');
+});
