f8eb2fa0a679db79606efde03101a0acb8f4ec8b
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-13 08:30:46 +1000

    adding routes and Express setup to server.js and added routes to apiroutes and to html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,18 @@
+// Dependencies and variables
 const path = require("path");
+
+module.exports = function (app){
+    
+    app.get("/exercise", function (req,res){
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+    app.get("/", function(req,res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    app.get("/stats", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+
+}
--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 // Dependencies and variables
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
-const PORT = 9000;
 
+// Setting up Express App
+const PORT = process.env.PORT || 9000;
 const app = express();
 
+// Sets up morgan to log req
+app.use(morgan("dev"));
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+// DB Mongo
+let MONGODB_URI = process.env.MONGODB_URI ||  "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// Creating routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // Start the server to begin listening
 app.listen(PORT, function() {
