a1a19b3d8c33c24aab2a6c950ee0459b3ac2e675
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-12 21:28:02 +1000

    Adding port and app.use

--- server.js
+++ server.js
@@ -2,7 +2,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const PORT = 9000;
 
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Start the server to begin listening
 app.listen(PORT, function() {
