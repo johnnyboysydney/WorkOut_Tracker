5e73a95762d4710f739ceed88829339e51e3cf1a
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-13 08:13:00 +1000

    added comments to workout.js

--- models/workout.js
+++ models/workout.js
@@ -41,14 +41,19 @@ const workoutSchema = new Schema(
     },
     {
         toJSON: {
+             // include any virtual properties when data is requested
             virtuals: true
         }
     }
 );
 
-
-
-
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+    }, 0);
+  });
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
